8276d6c089557321e42e0dd0ff8c74366d40727a
PrachiHejib <p.hejib@uea.ac.uk>
2020-05-01 16:56:58 +0100

    base diagram design

--- Script.R
+++ Script.R
@@ -1,6 +1,8 @@
+#find the "clear" command and try to run as a whole script
 
 install.packages("ggplot2")
 library(ggplot2)
+library(readxl)
 
 #Set a plot Region to define lottery rectangle
 plot(c(10, 100), c(0, 30), type = "n",
@@ -8,4 +10,22 @@ plot(c(10, 100), c(0, 30), type = "n",
 i <- 4*(0:10)
 abline(v=10, col="black")
 
+
+#plot from data 
+Lotteries_Code_2 <- read_excel("~/Documents/OneDrive_ University_of_East Anglia/UEA/Year_2/Paper_1 /Experiment /Lotteries_Code_2.xlsx")
+
+View(Lotteries_Code_2)   
+
+
        
+#Example of rectangles (using a data frame)
+df <- data.frame(
+  x = 1:100,
+  y = rep(1, 100),
+  z = Lotteries_Code_2$p1,
+  w = rep(1, 100)
+)
+
+ggplot(df, aes(x, y, width = w)) +
+  geom_tile(aes(fill = z), colour = "grey50")
+

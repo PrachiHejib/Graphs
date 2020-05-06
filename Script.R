e2d0d7a20e0877603c6d52f47ad7a03ab1d7e404
PrachiHejib <p.hejib@uea.ac.uk>
2020-05-06 15:08:48 +0100

    tidy script, add clear workspace command

--- Script.R
+++ Script.R
@@ -1,21 +1,15 @@
-#find the "clear" command and try to run as a whole script
+#Creating lotteries for experiment
+
+rm(list = ls(all = TRUE))
 
 install.packages("ggplot2")
 library(ggplot2)
 library(readxl)
 
-#Set a plot Region to define lottery rectangle
-plot(c(10, 100), c(0, 30), type = "n",
-     main = "Lottery Choices")
-i <- 4*(0:10)
-abline(v=10, col="black")
-
-
 #plot from data 
 Lotteries_Code_2 <- read_excel("~/Documents/OneDrive_ University_of_East Anglia/UEA/Year_2/Paper_1 /Experiment /Lotteries_Code_2.xlsx")
 
 View(Lotteries_Code_2)   
-
 
        
 #Example of rectangles (using a data frame)
@@ -27,5 +21,5 @@ df <- data.frame(
 )
 
 ggplot(df, aes(x, y, width = w)) +
-  geom_tile(aes(fill = z), colour = "grey50")
+  geom_tile(aes(fill = z), colour = "blue") 
 

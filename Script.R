d421ffc6798528e4abb2a79b4bcf29a20cd75b9c
PrachiHejib <p.hejib@uea.ac.uk>
2020-05-29 11:39:07 +0100

    change data frame to allow text on diagram

--- Script.R
+++ Script.R
@@ -7,17 +7,27 @@ library(ggplot2)
 library(readxl)
 library("RColorBrewer", lib.loc="/Library/Frameworks/R.framework/Versions/3.5/Resources/library")
 library("ggthemes", lib.loc="/Library/Frameworks/R.framework/Versions/3.5/Resources/library")
+library(reshape2)
+library(dplyr)
 
 #plot from data 
 Lotteries_Code_2 <- read_excel("~/Documents/OneDrive_ University_of_East Anglia/UEA/Year_2/Paper_1 /Experiment /Lotteries_Code_2.xlsx")
 
 View(Lotteries_Code_2)   
 
-#Example of rectangles (using a data frame)
+#create new data frame  with only lottery and p1
+lottery <- Lotteries_Code_2 %>% select(Lottery, p1)
+
+#rename coloumns
+lottery <- lottery %>% rename(roll=Lottery, payoff=p1)
+
+#transformation of "lottery" column to Roll and "p1" to Lottery.
+
+#Example of rectangles (using a data frame) (change to a generic form name Lottery)
 df <- data.frame(
   x = 1:100,
   y = rep(1, 100),
-  z = Lotteries_Code_2$p1,
+  z = lottery$payoff,
   w = rep(1, 100)
 )
 
@@ -25,13 +35,10 @@ a <- ggplot(df, aes(x, y, width = w))
 
 b <- a + geom_tile(aes(fill = z), show.legend = FALSE)
 c <- b + scale_fill_gradient(low="grey", high="grey50")
-c + labs(title ="", x = "", y = "") + theme_solid()
+c + labs(title ="", x = "", y = "") + theme_solid() 
 
 
-
-
-
-
- 
+#use median/average to label boxes  
+  
 #Find name/call/save graph/plot command
 

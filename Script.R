9b3d0c484b8836434683b7378f902d92c61a4751
Theodore Turocy <ted.turocy@gmail.com>
2020-06-12 11:09:26 +0100

    Convert to using geom_rect as more straightforward approach.

--- Script.R
+++ Script.R
@@ -13,8 +13,6 @@ library(dplyr)
 #plot from data 
 Lotteries_Code_2 <- read_excel("Lotteries_Code_2.xlsx")
 
-# View(Lotteries_Code_2)   
-
 #create new data frame  with only lottery and p1
 lottery <- Lotteries_Code_2 %>% select(Lottery, p1) 
 
@@ -24,18 +22,19 @@ lottery <- lottery %>% rename(roll=Lottery, payoff=p1)
 #use median/average to label boxes (creates a new table)
 medians <- lottery %>% group_by(payoff) %>% summarize(loc=median(roll), min=min(roll), max=max(roll))
 
-#create data frame for lotteries blocks 
-df <- data.frame(
-  x = 1:100,
-  y = rep(1, 100),
-  z = lottery$payoff,
-  w = rep(1, 100)
-)
+ggplot() +
+    # Draw the rectangles
+    geom_rect(data=medians,
+              mapping=aes(xmin=min, xmax=max, ymin=0, ymax=10, fill=payoff),
+              show.legend=FALSE) +
+    # Suppress axis labels
+    labs(title="", x="", y="") +
+    # Suppress the axis labels and borders
+    theme_solid() +
+    # Fix the aspect ratio so we can get long, thin strips
+    # Shape of the graph will be determined by ratio of ymax as passed to
+    # geom_rect and the maximum roll
+    coord_fixed() +
+    geom_text(aes(x=loc, y=5, label=payoff), data=medians)
 
-ggplot(df, aes(x, y)) +
-    geom_tile(aes(fill=z), show.legend=FALSE) +
-    scale_fill_gradient(low="grey", high="grey50") +
-    labs(title="", x="", y="") + theme_solid() +
-    geom_text(aes(x=loc, y=1, label=payoff), data=medians)
-
-ggsave("plot.pdf")
+ggsave("plot.pdf", width=5, height=1, units="in")

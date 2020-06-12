554d6b2df03ee7e393edebbf820b890b3bc1c31c
Theodore Turocy <ted.turocy@gmail.com>
2020-06-12 11:16:54 +0100

    Refactoring data and libraries:
    (1) Move lotteries data into CSV, more version-control friendly;
    (2) Clean up required libraries

--- Script.R
+++ Script.R
@@ -3,21 +3,17 @@
 #clear workspace
 rm(list = ls(all = TRUE))
 
-library(ggplot2)
-library(readxl)
-library(RColorBrewer)
+library(tidyverse)
 library(ggthemes)
-library(reshape2)
-library(dplyr)
 
 #plot from data 
-Lotteries_Code_2 <- read_excel("Lotteries_Code_2.xlsx")
+Lotteries_Code_2 <- read_csv("data/lotteries.csv")
 
-#create new data frame  with only lottery and p1
-lottery <- Lotteries_Code_2 %>% select(Lottery, p1) 
+#create new data frame  with only roll and p1
+lottery <- Lotteries_Code_2 %>% select(roll, p1) 
 
 #rename coloumns
-lottery <- lottery %>% rename(roll=Lottery, payoff=p1)
+lottery <- lottery %>% rename(payoff=p1)
 
 #use median/average to label boxes (creates a new table)
 medians <- lottery %>% group_by(payoff) %>% summarize(loc=median(roll), min=min(roll), max=max(roll))

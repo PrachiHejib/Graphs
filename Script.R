112b9cd7ab4eb44fa30535d6d573d59c1efc4715
Theodore Turocy <ted.turocy@gmail.com>
2020-06-12 11:58:48 +0100

    Simplify pipelines for processing lotteries data.

--- Script.R
+++ Script.R
@@ -6,21 +6,20 @@ rm(list = ls(all = TRUE))
 library(tidyverse)
 library(ggthemes)
 
-#plot from data 
-Lotteries_Code_2 <- read_csv("data/lotteries.csv")
 
-#create new data frame  with only roll and p1
-lottery <- Lotteries_Code_2 %>% select(roll, p1) 
+# For now - plot lottery p1 only
+lottery <-
+    read_csv("data/lotteries.csv") %>%
+    select(roll, p1) %>%
+    rename(payoff=p1)
 
-#rename coloumns
-lottery <- lottery %>% rename(payoff=p1)
-
-#use median/average to label boxes (creates a new table)
-medians <- lottery %>% group_by(payoff) %>% summarize(loc=median(roll), min=min(roll), max=max(roll))
+# Compute coordinates for graph elements
+coordinates <- lottery %>% group_by(payoff) %>%
+    summarize(loc=median(roll), min=min(roll), max=max(roll))
 
 ggplot() +
     # Draw the rectangles
-    geom_rect(data=medians,
+    geom_rect(data=coordinates,
               mapping=aes(xmin=min, xmax=max, ymin=0, ymax=10, fill=payoff),
               show.legend=FALSE) +
     # Suppress axis labels
@@ -31,6 +30,6 @@ ggplot() +
     # Shape of the graph will be determined by ratio of ymax as passed to
     # geom_rect and the maximum roll
     coord_fixed() +
-    geom_text(aes(x=loc, y=5, label=payoff), data=medians)
+    geom_text(aes(x=loc, y=5, label=payoff), data=coordinates)
 
 ggsave("plot.pdf", width=5, height=1, units="in")
